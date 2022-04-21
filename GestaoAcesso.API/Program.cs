7fa22f4b02f05b47b4bf33e588ecb6f428a372f4
Hugo <hugo.flausino@hotmail.com>
2022-04-21 10:10:45 -0300

    Configurada secrets no projeto

--- GestaoAcesso.API/Program.cs
+++ GestaoAcesso.API/Program.cs
@@ -21,6 +21,8 @@ namespace GestaoAcesso.API
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+            .ConfigureAppConfiguration((context, builder) =>
+            builder.AddUserSecrets<Program>());
     }
 }
